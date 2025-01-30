d759cec3b5adfe78e48c5f0277244d409d67c49a
Raghul KB <raghulbalamurugan15@gmail.com>
2025-01-29 18:51:13 -0600

    register-page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Views/**/*.html", // Scan all .html files in Views folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
